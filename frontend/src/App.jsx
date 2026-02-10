e3510f99c1ec95f42294a0c10be69d39dc91a219
Gabriel Rodrigues <gabriel.r.lacerda@hotmail.com>
2026-02-10 11:15:53 -0300

    criando projeto com vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="logo">
+          <a href="">Kaillany®</a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
